957bf4a97894be29df9c1abdb2ea50a9c74adb02
blank-bit <1696431161@qq.com>
2021-03-08 19:43:25 +0800

    new

---
+++ test.java
@@ -0,0 +1,21 @@
+/*********************************************
+ @Author       : Mr.Wang
+ @Date         : 2021-01-15 00:48:43
+ @FilePath     : /test.java
+ @Description  : message
+*********************************************/
+class cat{
+    public cat(){
+        System.out.println("cat");
+    }
+}
+
+public class test {
+    public test(){
+        System.out.println("test");
+    }
+
+    public static void main(String[] args) {
+        new test();        
+    }
+}
